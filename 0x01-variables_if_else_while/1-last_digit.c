7091b39b936b58109a3d48141a868d213cf09f50
newsybani <nwaubanigoodnews@gmail.com>
2022-09-18 19:43:24 -0700

    last command

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	printf("Last digit of %d is %d and is greater than 5\n", n, number);
 	}
 	else if (number == 0)
-	{							
+	{			
 		printf("Last digit of %d is %d and is 0\n", n, number);
 	}
 	else if (number < 6)
