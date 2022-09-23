736936d3d36b3ced345e59fdc231b0551002d33b
newsybani <nwaubanigoodnews@gmail.com>
2022-09-23 03:32:44 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar (alphabet);
+		alphabet++;
+	
+	}
+	_putchar ('\n');
+}
