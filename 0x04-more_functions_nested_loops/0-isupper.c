48c58b7f63e2da4edf87cf61fc6a94bfd41153fd
newsybani <nwaubanigoodnews@gmail.com>
2022-09-24 12:48:27 -0700

    Functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks the input to see if it is upper or lower
+ *  @c: the input character
+ *  Return:  1 if uppercase 0 if lower
+ */
+int _isupper(int c)
+{
+		if (c >= 65 && c <= 90)
+				{
+							return (1);
+								}
+			else
+					{
+								return (0);
+									}
+}
